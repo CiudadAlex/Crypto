8b84f786528338a67e654a29605fea46cc16d59d
CiudadAlex <ciudad.alex@gmail.com>
2026-04-14 00:04:19 +0200

    Crypto

--- src/main/java/org/leviatanplatform/crypto/engine/layers/RevolutionLayer.java
+++ src/main/java/org/leviatanplatform/crypto/engine/layers/RevolutionLayer.java
@@ -1,11 +1,25 @@
 package org.leviatanplatform.crypto.engine.layers;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.util.List;
+
 public class RevolutionLayer implements Layer {
 
     @Override
     public byte[] encrypt(byte[] chunk, byte[] effectiveKey) {
+
+        ByteArrayInputStream baisEffectiveKey = new ByteArrayInputStream(effectiveKey);
+        DataInputStream disEffectiveKey = new DataInputStream(baisEffectiveKey);
+        long value = disEffectiveKey.readLong();
+
+
         // FIXME implement
         return new byte[0];
+    }
+
+    private List<Integer> getShuffledIndices() {
+
     }
 
     @Override
