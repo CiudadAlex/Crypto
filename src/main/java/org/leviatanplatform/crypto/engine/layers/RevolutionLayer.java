c2fb4dbeeb068ace84379412467fb31b0b87e9c1
CiudadAlex <ciudad.alex@gmail.com>
2026-04-13 21:46:36 +0200

    Crypto

---
+++ src/main/java/org/leviatanplatform/crypto/engine/layers/RevolutionLayer.java
@@ -0,0 +1,16 @@
+package org.leviatanplatform.crypto.engine.layers;
+
+public class RevolutionLayer implements Layer {
+
+    @Override
+    public byte[] encrypt(byte[] chunk, byte[] effectiveKey) {
+        // FIXME implement
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] decrypt(byte[] chunk, byte[] effectiveKey) {
+        // FIXME implement
+        return new byte[0];
+    }
+}
