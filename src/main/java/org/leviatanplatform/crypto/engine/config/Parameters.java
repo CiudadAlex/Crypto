6a0cf1ee405071acf796e4d4c7722ba0e4d8d068
CiudadAlex <ciudad.alex@gmail.com>
2026-04-13 16:44:28 +0200

    Crypto

---
+++ src/main/java/org/leviatanplatform/crypto/engine/config/Parameters.java
@@ -0,0 +1,10 @@
+package org.leviatanplatform.crypto.engine.config;
+
+public class Parameters {
+
+    private final int lengthBlockBytes = 100 * 1024;
+
+    public int getLengthBlockBytes() {
+        return lengthBlockBytes;
+    }
+}
