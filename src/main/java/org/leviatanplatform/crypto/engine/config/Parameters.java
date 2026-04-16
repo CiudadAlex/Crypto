a2415d777a2b6a5a3c7036741ea485df1b7a8258
CiudadAlex <ciudad.alex@gmail.com>
2026-04-16 18:28:50 +0200

    Crypto

--- src/main/java/org/leviatanplatform/crypto/engine/config/Parameters.java
+++ src/main/java/org/leviatanplatform/crypto/engine/config/Parameters.java
@@ -5,36 +5,54 @@ import java.util.Properties;
 
 public class Parameters {
 
-    private static final Parameters instance = new Parameters();
+    private static final String CONFIG_FILE_NAME = "config.properties";
+    private static final Parameters instance = load();
 
     public static Parameters getInstance() {
         return instance;
     }
 
-    private final int lengthBlockBytes = 100 * 1024;
+    private final int lengthBlockBytes;
+    private final String name;
+    private final String surname;
+    private final String birthdate;
+
+    public Parameters(int lengthBlockBytes, String name, String surname, String birthdate) {
+        this.lengthBlockBytes = lengthBlockBytes;
+        this.name = name;
+        this.surname = surname;
+        this.birthdate = birthdate;
+    }
 
     public int getLengthBlockBytes() {
         return lengthBlockBytes;
     }
 
-    private static void load() throws Exception {
+    private static Parameters load() {
 
         // FIXME finish
 
         Properties props = new Properties();
 
-        try (InputStream input = Parameters.class
-                .getClassLoader()
-                .getResourceAsStream("config.properties")) {
+        try (InputStream input = Parameters.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME)) {
 
             if (input == null) {
-                throw new RuntimeException("File not found");
+                throw new RuntimeException("File " + CONFIG_FILE_NAME + " not found");
             }
 
             props.load(input);
-        }
 
-        String valor = props.getProperty("birthdate");
-        System.out.println(valor);
+            String strLengthBlockBytes = props.getProperty("lengthBlockBytes");
+            String name = props.getProperty("name");
+            String surname = props.getProperty("surname");
+            String birthdate = props.getProperty("birthdate");
+
+            int lengthBlockBytes = Integer.parseInt(strLengthBlockBytes);
+
+            return new Parameters(lengthBlockBytes, name, surname, birthdate);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Exception loading configuration", e);
+        }
     }
 }
