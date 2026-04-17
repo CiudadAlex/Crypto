b62420e6114e069911c0ee90377c60c6897b9b24
CiudadAlex <ciudad.alex@gmail.com>
2026-04-18 01:16:40 +0200

    Crypto

--- src/main/java/org/leviatanplatform/crypto/Main.java
+++ src/main/java/org/leviatanplatform/crypto/Main.java
@@ -11,22 +11,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String key = "password-key-to-encrypt";
-        encryptAndDecrypt("Oasis_Morning_Glory","mp3", key);
-
-        FileContentExtractor.extract(PATH_PLAYGROUND + "/RandomFile.key");
-
-        // FIXME key importada de fichero
-    }
-
-    private static void encryptAndDecrypt(String fileName, String extension, String key) throws IOException {
+        String fileName = "Oasis_Morning_Glory";
+        String extension = "mp3";
 
         String pathFilePlain = PATH_PLAYGROUND + "/" + fileName + "." + extension;
         String pathFileEncrypted = pathFilePlain + ".encrypted";
+        String pathFileDecrypted = pathFileEncrypted + ".decrypted" + "." + extension;
+
+        String key = "password-key-to-encrypt";
+        //byte[] key = FileContentExtractor.extract(PATH_PLAYGROUND + "/RandomFile.key");
 
         CryptoEngine.encryptFile(pathFilePlain, pathFileEncrypted, key);
-
-        String pathFileDecrypted = pathFileEncrypted + ".decrypted" + "." + extension;
         CryptoEngine.decryptFile(pathFileEncrypted, pathFileDecrypted, key);
     }
 }
